3d255e9afd07cc22dc60995f2da1ff79e3057a74
Aaron <aaron@Aarons-Air.lan>
2022-05-23 16:12:59 -0400

    database and table creation

---
+++ database/migration.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS meal_tracker;
+
+CREATE DATABASE meal_tracker;
+
+\c meal_tracker;
+
+CREATE TABLE meals (
+    meal_id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    description varchar(255),
+    prep_time int
+);
