1d7b754dca230230e8a135f5da68657cf76e3cd5
Aaron <aaron@Aarons-Air.lan>
2022-05-23 17:52:49 -0400

    added get one

--- server.js
+++ server.js
@@ -4,9 +4,32 @@ import pool from './database/connect.js';
 const app = express();
 dotenv.config()
 
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+app.use(express.static("public"))
+
+//get all
 app.get('/api/meals', async(req,res) => {
-    const data = await pool.query('SELECT * FROM meals;')
-    res.json(data.rows)
+    try {
+        const data = await pool.query('SELECT * FROM meals;')
+        res.json(data.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
 })
 
-app.listen(8000)
+//get one
+app.get('/api/meals/:id', async(req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM meals WHERE meal_id = $1;', [req.params.id])
+        res.json(data.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//create server
+app.listen(PORT, () => {
+    console.log('listening on port:', PORT)
+})
