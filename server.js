541f1c3a93bd376376ff2866765b2d174d6c9f05
Aaron <aaron@Aarons-Air.lan>
2022-05-23 18:44:20 -0400

    added delete one

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import express from 'express';
 import pool from './database/connect.js';
 const app = express();
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 8000
 
-app.use(express.json())
-app.use(express.static("public"))
+app.use(express.json());
+app.use(express.static("public"));
 
 //get all
 app.get('/api/meals', async(req,res) => {
@@ -17,7 +17,7 @@ app.get('/api/meals', async(req,res) => {
     } catch (err) {
         console.error(err.message)
     }
-})
+});
 
 //get one
 app.get('/api/meals/:id', async(req,res) => {
@@ -27,13 +27,13 @@ app.get('/api/meals/:id', async(req,res) => {
     } catch (err) {
         console.error(err.message)
     }
-})
+});
 
 //post one
 app.post('/api/meals', async(req,res) => {
     try {
         if (![req.body.name, req.body.ingredients, req.body.prep_time].includes(undefined)) {
-            const data = await pool.query('INSERT INTO meals (name, ingredients, prep_time) VALUES ($1, $2, $3);', [req.body.name, req.body.ingredients, req.body.prep_time])
+            const data = await pool.query('INSERT INTO meals (name, ingredients, prep_time) VALUES ($1, $2, $3) RETURNING *;', [req.body.name, req.body.ingredients, req.body.prep_time])
             res.json(data.rows)
         } else {
             res.send('Information is missing')
@@ -41,7 +41,7 @@ app.post('/api/meals', async(req,res) => {
     } catch (err) {
         console.error(err.message)
     }
-})
+});
 
 //update one
 app.patch('/api/meals/:id', async(req,res) => {
@@ -58,9 +58,19 @@ app.patch('/api/meals/:id', async(req,res) => {
     } catch (err) {
         console.error(err.message)
     }
-})
+});
+
+//delete one
+app.delete('/api/meals/:id', async(req,res) => {
+    try {
+        const deleted = await pool.query('DELETE FROM meals WHERE meal_id = $1;', [req.params.id])
+        res.send('Meal has been deleted')
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 
 //create server
 app.listen(PORT, () => {
     console.log('listening on port:', PORT)
-})
+});
