92ff8c13f45f0243f20b883665e0176f3a096bf4
Aaron <aaron@Aarons-Air.lan>
2022-05-24 13:40:22 -0400

    added long prep and event listeners

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
     homePage()
-    //shortPrep()
-    //mediumPrep()
 })
+
+const short = document.querySelector('#short')
+const medium = document.querySelector('#medium')
+const long = document.querySelector('#long')
 
 const GET = async () => {
     let result = await fetch('http://localhost:8000/api/meals')
@@ -56,8 +58,29 @@ const mediumPrepData = (data) => {
     divContent(arr)
 }
 
+const longPrep = async () => {
+    const data = await GET();
+    longPrepData(data)
+}
+
+const longPrepData = (data) => {
+    let obj = {};
+    let arr = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].prep_time >= 120) {
+            obj.name = data[i].name
+            obj.ingredients = data[i].ingredients
+            obj.prep_time = data[i].prep_time
+            arr.push(obj)
+        }
+    }
+    createDiv(arr.length)
+    divContent(arr)
+}
+
+
 const createDiv = (num) => {
-    let content = document.querySelector('.content')
+    let content = document.querySelector('.container')
     for (let i = 1; i <= num; i++) {
         let div = document.createElement('div')
         div.id = i
@@ -69,6 +92,7 @@ const createDiv = (num) => {
 const divContent = (data) => {
     let card = document.querySelectorAll('.card')
     for (let i = 0; i < card.length; i++) {
+        let current = card[i]
         let h1 = document.createElement('h1')
         let p = document.createElement('p')
         let p2 = document.createElement('p')
@@ -79,7 +103,24 @@ const divContent = (data) => {
 
         p.appendChild(p2)
         h1.appendChild(p)
-        card[i].appendChild(h1)
+        current.appendChild(h1)
     }
 }
 
+short.addEventListener('click', () => {
+    let container = document.querySelector('.container')
+    container.innerHTML = ''
+    shortPrep()
+})
+
+medium.addEventListener('click', () => {
+    let container = document.querySelector('.container')
+    container.innerHTML = ''
+    mediumPrep()
+})
+
+long.addEventListener('click', () => {
+    let container = document.querySelector('.container')
+    container.innerHTML = ''
+    longPrep()
+})
