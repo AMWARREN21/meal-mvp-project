6199c43182d6d16e0aa7340aa07b4bee017d6d6b
Aaron <aaron@Aarons-Air.lan>
2022-05-24 13:44:08 -0400

    added more entities

--- database/seed.sql
+++ database/seed.sql
@@ -1,1 +1,3 @@
 INSERT INTO meals (name, ingredients, prep_time) VALUES ('Spaghetti with Sausages', 'Spaghetti noodles, Traditional Sauce, Ground Beef, Sausages', 40);
+INSERT INTO meals (name, ingredients, prep_time) VALUES ('Chicken', 'Chicken, Assortment of Seasonings', 60);
+INSERT INTO meals (name, ingredients, prep_time) VALUES ('Steak', 'Steak, Marinade', 180);
