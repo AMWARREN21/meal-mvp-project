55ab82d753c8207db9778f26f58ff6337e4442d3
Aaron <aaron@Aarons-Air.lan>
2022-05-25 19:14:55 -0400

    added more entities

--- database/seed.sql
+++ database/seed.sql
@@ -1,3 +1,7 @@
 INSERT INTO meals (name, ingredients, prep_time) VALUES ('Spaghetti with Sausages', 'Spaghetti noodles, Traditional Sauce, Ground Beef, Sausages', 40);
 INSERT INTO meals (name, ingredients, prep_time) VALUES ('Chicken', 'Chicken, Assortment of Seasonings', 60);
 INSERT INTO meals (name, ingredients, prep_time) VALUES ('Steak', 'Steak, Marinade', 180);
+INSERT INTO meals (name, ingredients, prep_time) VALUES ('Overnight Oats', 'Oats, Milk, Chia Seeds, Maple Syrup', 10);
+INSERT INTO meals (name, ingredients, prep_time) VALUES ('Ramen', 'Ramen and Water', 10);
+INSERT INTO meals (name, ingredients, prep_time) VALUES ('Chili Cheese Dogs', 'Chili, Cheese Dogs, Buns, Cheese', 15);
+INSERT INTO meals (name, ingredients, prep_time) VALUES ('Eggs', 'Eggs', 10);
